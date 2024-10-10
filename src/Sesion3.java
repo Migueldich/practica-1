14baa0d0a34371dcd44121213a9cf1fc11d97f50
Miguel <miguel.aa0210@gmail.com>
2024-10-10 20:21:29 +0200

    he arreglado problemas de la sesion3 y me falta solo la funcion de esPrimoRecursivo en la sesion4

--- src/Sesion3.java
+++ src/Sesion3.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 class Sesion3 {
@@ -6,6 +5,14 @@ class Sesion3 {
     static int menu(int num1) {
         // primera funcion, lo que hace es comprobar que un numero esta comprendido entre 1 y 6 y devuelve ese numero
         Scanner uno = new Scanner(System.in);
+        System.out.println("1. Comprobar si un numero es primo \n" +
+                "2. Escribir los n primeros numeros primos \n" +
+                "3. Escribir numeros primos gemelos entre dos valores\n" +
+                "4. Numero par como suma de dos primos\n" +
+                "5. Comprobar si un numero es primo de manera recursiva\n" +
+                "6. Calcular potencia \n" +
+                "0. Finalizar la ejecucion\n " +
+                "Escriba un numero comprendido entre 1 y 6: ");
         num1 = uno.nextInt();
         // lee el numero que le damos
         while (num1 < 0 || num1 > 6) {
@@ -16,7 +23,7 @@ class Sesion3 {
             //te vuelve a pedir el numero
         }
         return num1;
-       // si el numero esta comprendido entre 1 y 6 lo devuelve
+        // si el numero esta comprendido entre 1 y 6 lo devuelve
     }
 
     static int leerNumero(int n1, int n2) {
@@ -42,11 +49,12 @@ class Sesion3 {
                 //si divido el numero entre cualquier menor a este y el resto es cero, ya no es primo (omitiendo el 1)
                 primo = false;
                 return primo;
+                //me devuelve false
             }
         }
         if (primo)
             return primo;
-
+        //como es de tipo booleano me devuelve el valor de primo (en este caso true)
         return primo;
     }
 
@@ -55,35 +63,35 @@ class Sesion3 {
         int num = 2;
         int j = 1;
         while (j <= n) {
+            //bucle que me sirve de contador hasta llegar al numero de numeros primos indicado
             boolean esPrimo = true;
             for (int m = 2; m < num; m++) {
                 if (num % m == 0)
                     esPrimo = false;
             }
+            //hace lo mismo que la funcion esPrimo
             if (esPrimo) {
                 System.out.printf("%8d", num);
+                //escribe el numero primo con formato en caso de que sea primo
                 if (j % 4 == 0)
                     System.out.printf("\n");
+                //cada vez que halla 4 numeros primos en la misma linea se hace un salto de linea
                 j++;
+                //al ser primo, añado uno al contador de numeros primos que tengo
             }
             num++;
+            //sea primo o no, añado uno a la variable num para que mire si el siguiente numero es primo
         }
         System.out.println("\n");
+        //simplemente para que quede bonito
     }
 
     public static void main(String[] args) {
-        Scanner dos = new Scanner(System.in);
         int num5 = 1;
         while (num5 != 0) {
-            System.out.print("1. Comprobar si un numero es primo \n" +
-                    "2. Escribir los n primeros numeros primos \n" +
-                    "3. Escribir numeros primos gemelos entre dos valores\n" +
-                    "4. Numero par como suma de dos primos\n" +
-                    "5. Comprobar si un numero es primo de manera recursiva\n" +
-                    "6. Calcular potencia \n" +
-                    "0. Finalizar la ejecucion\n " +
-                    "Escriba un numero comprendido entre 1 y 6: ");
+            // como queremos que el codigo pare cuando escribamos 0, el bucle se cumplira mientras el valor que demos no sea 0, y empieza en uno para que la primera vez funcione al menos.
             num5 = menu(num5);
+            //un switch simple en el que actua la funcion menu, para ver si el numero es valido.
             switch (num5) {
                 case 0:
                     System.out.println("Adios");
--- src/Sesion4.java
+++ src/Sesion4.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Sesion4 {
     public static void primosGemelos() {
         int m = Sesion3.leerNumero(1, 500);
@@ -18,28 +17,78 @@ public class Sesion4 {
         int j=0;
         for (int i = 2; i<n1; i++){
             j= n1-i;
-            if (Sesion3.esPrimo(i) == true && Sesion3.esPrimo(j)== true && j!=1 && j>i){
+            if (Sesion3.esPrimo(i) == true && Sesion3.esPrimo(j)== true && j!=1 && j>=i){
                 System.out.println( i + " + " + j + " = " + n1);
             }
         }
 
     }
+    public static boolean esPrimoRecursivo(int n1){
+        while (n1 != 0) {
 
-    public static double potencia(double num1, double exponente) {
-        double multiplo = 1;
-        while (exponente > 0) {
-            return num1 * potencia(num1, exponente--);
         }
-        return potencia(num1, exponente);
+        n1=0;
+        return true;
+    }
+
+    public static double potencia(double base, double exponente) {
+        if (base == 0){
+            return 0;
+        }else if ( exponente == 0){
+            return 1;
+        }else if (exponente!=1){
+        return  base * potencia(base, exponente-1);
+        }else{
+            return base;
+        }
 
     }
 
     public static void main(String[] args) {
-        int n3 = 1;
-        mostrarSumasDePrimos(n3);
-        int n1 = Sesion3.leerNumero(-100, 100);;
-        int n2 = Sesion3.leerNumero(0, 10);;
-        potencia(n1, n2);
+        Scanner dos = new Scanner(System.in);
+        int num5 = 1;
+        while (num5 != 0) {
+            // como queremos que el codigo pare cuando escribamos 0, el bucle se cumplira mientras el valor que demos no sea 0, y empieza en uno para que la primera vez funcione al menos.
+            num5 = Sesion3.menu(num5);
+            //un switch simple en el que actua la funcion menu, para ver si el numero es valido.
+            switch (num5) {
+                case 0:
+                    System.out.println("Adios");
+                    break;
+                case 1:
+                    int n1 = Sesion3.leerNumero(1, 1000);
+                    if (Sesion3.esPrimo(n1) == true) {
+                        System.out.println("el numero " + n1 + " es primo");
+                    } else {
+                        System.out.println(" el numero " + n1 + " no es primo ");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Escriba cuantos numeros primos quiere: ");
+                    int contadorNumPrimos = Sesion3.leerNumero(5, 20);
+                    Sesion3.escribePrimos(contadorNumPrimos);
+                    break;
+                case 3:
+                    primosGemelos();
+                    break;
+                case 4:
+                    int n3 = 1;
+                    mostrarSumasDePrimos(n3);
+                    break;
+                case 5:
+                    System.out.println("Comprobar si un numero es primo de manera recursiva");
+                    break;
+                case 6:
+                    System.out.println("Calcular potencia ");
+                    int n4 = Sesion3.leerNumero(-100, 100);
+                    int n2 = Sesion3.leerNumero(0, 10);
+                    System.out.println( n4 + " elevado a " + n2 + " es " +potencia(n4, n2));
+                    break;
+
+            }
+        }
+
+
     }
 
 
